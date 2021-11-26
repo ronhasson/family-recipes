3452ded520d12a06b11819ff9ecb46986901d8b4
Ron <ron23231@gmail.com>
2021-11-26 17:43:24 +0200

    viewRecipe is done

--- src/routes/Recipe.jsx
+++ src/routes/Recipe.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState, useContext, Suspense } from "react";
 import placeholder from "../img/foodPlaceholder.jpg";
 import styles from "./recipeForm.module.css";
+import rstyles from "./recipe.module.css";
 import { button, white } from "../commonStyles.module.css";
 import { db } from "../firebase";
 import { collection, doc } from "firebase/firestore";
@@ -8,6 +9,7 @@ import { UserContext } from "../App.js";
 import { useParams } from "react-router-dom";
 import { useDocumentOnce } from 'react-firebase-hooks/firestore';
 import Loading from "../components/loading";
+import timerImg from "../img/timer.svg";
 
 function RecipeForm() {
     const user = useContext(UserContext);
@@ -54,24 +56,51 @@ function RecipeForm() {
                 <Suspense fallback={<Loading />}>
                     <div ref={mainRef} className={styles.formContainer} >
                         <img ref={imageRef} src={placeholder} alt="" />
-                        <div>
-                            <div>{value.data().tags.map((tag, i) => {
-                                return (<span key={i + tag}>#{tag}</span>)
-                            })}</div>
+                        <div className={rstyles.content}>
+                            <div className={rstyles.tags}>
+                                {value.data().tags.map((tag, i) => {
+                                    return (<span key={i + tag}>#{tag}</span>)
+                                })}
+                            </div>
                             <h1>{value.data().name}</h1>
-                            <p>{value.data().desc}</p>
-                            <p>
-                                Prep:
-                                {value.data().prep[0] > 0 && <span> {value.data().prep[0]} days, </span>}
-                                {value.data().prep[1] > 0 && <span>{value.data().prep[1]} hours, </span>}
-                                {!(value.data().prep[0] + value.data().prep[0] !== 0 && value.data().prep[2] === 0) && <span>{value.data().prep[2]} min </span>}
-                            </p>
-                            <p>
-                                Cooking:
-                                {value.data().cook[0] > 0 && <span> {value.data().cook[0]} days, </span>}
-                                {value.data().cook[1] > 0 && <span>{value.data().cook[1]} hours, </span>}
-                                {!(value.data().cook[0] + value.data().cook[0] !== 0 && value.data().cook[2] === 0) && <span>{value.data().cook[2]} min </span>}
-                            </p>
+                            <div className={rstyles.recipeCont}>
+                                <p>{value.data().desc}</p>
+                                <div className={rstyles.time}>
+                                    <img src={timerImg} alt="time" />
+                                    <p>
+                                        Prep:
+                                        {value.data().prep[0] > 0 && <span> {value.data().prep[0]} days, </span>}
+                                        {value.data().prep[1] > 0 && <span>{value.data().prep[1]} hours, </span>}
+                                        {!(value.data().prep[0] + value.data().prep[0] !== 0 && value.data().prep[2] === 0) && <span>{value.data().prep[2]} min </span>}
+                                    </p>
+                                    <p>
+                                        Cooking:
+                                        {value.data().cook[0] > 0 && <span> {value.data().cook[0]} days, </span>}
+                                        {value.data().cook[1] > 0 && <span>{value.data().cook[1]} hours, </span>}
+                                        {!(value.data().cook[0] + value.data().cook[0] !== 0 && value.data().cook[2] === 0) && <span>{value.data().cook[2]} min </span>}
+                                    </p>
+                                </div>
+                                <h3>Ingredients</h3>
+                                <div className={rstyles.ingreds}>
+                                    {value.data().ingredients.map((ing, i) => {
+                                        return (
+                                            <label key={"ing" + ing + i}>
+                                                <input type="checkbox" /><span>{ing}</span>
+                                            </label>
+                                        )
+                                    })}
+                                </div>
+                                <h3>Directions</h3>
+                                <div>{value.data().instructions.map((ins, i) => {
+                                    return (
+                                        <div className={rstyles.step}>
+                                            {value.data().instructions.length > 1 && <span>{i + 1}</span>}
+                                            <p dangerouslySetInnerHTML={{ __html: ins }}></p>
+                                        </div>
+                                    )
+                                })}
+                                </div>
+                            </div>
                         </div>
                         <span className={styles.padding}></span>
                     </div>
