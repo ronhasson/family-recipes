eea60802807433c90e114a0cd03a51bf5356eec1
Ron <ron23231@gmail.com>
2021-12-03 21:12:01 +0200

    profile page now displays members + style

--- src/profileGetter.js
+++ src/profileGetter.js
@@ -1,14 +1,25 @@
 import {
     db
-} from "../firebase";
+} from "./firebase";
 import {
     doc,
     getDoc
 } from "firebase/firestore";
 
 async function getProfile(id) {
-
-
+    let local = getUserLocal(id);
+    if(local && expValid(local.exp,30)){
+        console.log("local ", local);
+        return local;
+    }
+    let server = await getUserServer(id);
+    if(server){
+        console.log("server ", server);
+        updateLocalUser(id,server);
+        return server;
+    }
+    console.log("user not found");
+    return undefined;
 }
 
 function getAllLocal() {
@@ -17,14 +28,21 @@ function getAllLocal() {
 
 function getUserLocal(id) {
     let u = getAllLocal();
+    if(!u){
+        return undefined
+    }
     return u[id];
 }
 
 function updateLocalUser(id, data) {
     let all = getAllLocal();
+    if(!all){
+        all = {};
+    }
     let newData = data;
-    newData.exp = new Date().toISOString;
+    newData.exp = new Date().toISOString();
     all[id] = newData;
+    console.log(all);
     localStorage.setItem("users", JSON.stringify(all));
 }
 
@@ -33,5 +51,13 @@ async function getUserServer(id) {
     const docSnap = await getDoc(docRef);
 
     return docSnap.data();
+}
+
+function expValid(date, days) {
+    const today = new Date();
+    const expDate = new Date(date);
+
+    let diff = (today-expDate)/1000/60/60/24;
+    return diff<days;
 }
 export default getProfile;
--- src/routes/Group.jsx
+++ src/routes/Group.jsx
@@ -2,28 +2,48 @@ import { useParams } from "react-router-dom";
 import Avatar from "../components/avatar";
 import { GroupContext } from "../App.js";
 import { useContext, useEffect, useState } from "react";
+import profileGet from "../profileGetter";
+import styles from "./group.module.css";
+import { useRef } from "react/cjs/react.development";
 
 function Group() {
     let urlParams = useParams();
     const groups = useContext(GroupContext);
     const [group, setGroup] = useState(groups.docs.filter(doc => doc.id === urlParams.id)[0]);
     const [groupName, setGName] = useState(group.data().name);
+    const [members, setMembers] = useState([]);
+    const titleInputRef = useRef();
     useEffect(() => {
         setGName(group.data().name);
     }, [group]);
     useEffect(() => {
         setGroup(groups.docs.filter(doc => doc.id === urlParams.id)[0]);
     }, [groups, urlParams.id]);
+    useEffect(()=>{
+        group.data().members.map(async(member, i)=>{
+            let d = await profileGet(member);
+            let m = [...members];
+            m[member] = d;
+            // console.log(members)
+            setMembers(m)
+        })
+    },[group]);
+    useEffect(()=>{
+        // titleInputRef.current.style.width = groupName.length+0.5 + "ch";
+    },[groupName])
     return (
         <div>
-            <h2><Avatar size="120%" rand={groupName + "1234567890"} /><input type="text" onChange={(e) => { setGName(e.target.value) }} value={groupName} /></h2>
-            <span>👑{group.data().owner}</span>
+            <h2 className={styles.title}><Avatar size="120%" rand={groupName + "1234567890"} /><div className={styles.titleContainer}><input ref={titleInputRef} disabled type="text" onChange={(e) => { setGName(e.target.value) }} value={groupName} /><span>{groupName}</span></div></h2>
+            <span className={styles.owner}>👑 {Object.keys(members).length > 0 && members[group.data().owner].name}</span>
             <div><input type="email" /><button>send invite</button></div>
-            <h4>Members:</h4>
+            <h4 className={styles.membersListTitle}>Members</h4>
             <div>
-                {group.data().members.map((member, i) => {
+                {members && Object.keys(members).map((key, i) => {
+                    let member = members[key];
+                    // console.log(member);
+                    // console.log(members);
                     return (
-                        <span key={"m" + i}>{member}</span>
+                        <span className={styles.member} key={"m" + i}>{member.name}</span>
                     )
                 })}
             </div>
